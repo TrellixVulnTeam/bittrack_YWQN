cb195c74a2407a212bab65561d83985e7c07b271
Rodo <rodonguyendd@gmail.com>
2022-06-30 13:49:09 +1000

    a

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from decouple import config
 
 API_KEY = config('API_KEY_binance_read', default='')
 SECRET_KEY = config('SECRET_KEY_binance_read', default='')
-print(API_KEY, SECRET_KEY)
-
 
 binance = ccxt.binance({
     'id': 'binance',
     'apiKey': API_KEY,
     'secret': SECRET_KEY
 })
+
+print(binance.apiKey)
