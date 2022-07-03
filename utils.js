e0ff90d6fb4f24caeac7bdbcef32b2c590588dc8
Rodo <rodonguyendd@gmail.com>
2022-07-04 00:14:42 +1000

    .

--- utils.js
+++ utils.js
@@ -1,5 +1,14 @@
 
-
+function handleInputLength(prices, length) {
+    if (prices.length < length) {
+        throw `closedPrice array does not have enough items, closedPrice.length=${prices.length} < ${length}`;    
+    }
+    else if (prices.length >length) {
+        console.log("closedPrice is longer than needed. Slice is implemented but this may bring undesirable result.");
+        prices = prices.slice(0,length);
+    } 
+    return prices;
+}
 
 async function getBalance(account) {
     const balance = await account.fetchBalance();
@@ -13,26 +22,18 @@ async function getApiStatus(account) {
 
 /**
  * @param
- *      closedPrices: an array of prices desirably of length 'length'  
+ *      closedPrices: an array of prices, desirably of length 'length'. If its length longer than 'length', the array will be trimmed off.
  *      length: the length of RSI calculated  
  */
 function calculateRsi(closedPrices, length=14) {
-    var prices = closedPrices;
-    
-    if (closedPrices.length < length) {
-        console.log(`closedPrice array does not have enough items, closedPrice.length=${closedPrices.length} < ${length}`);
-        return;
-    }
-    else if (closedPrices.length >length) {
-        console.log("closedPrice is longer than needed. Slice is implemented but this may bring undesirable result.");
-        prices = closedPrices.slice(0,length);
-    } 
+    var prices = handleInputLength(closedPrices, length);
 
+    // Calculating RSI
     // var tolerance = 10e-10;
     var sumGain = 0;
     var sumLoss = 0;
 
-    for (let i = 1; i < prices.length; i++) {
+    for (let i = 1; i < length; i++) {
         let difference = prices[i] - prices[i - 1];
         if (difference >= 0)
             sumGain += difference;
@@ -45,5 +46,19 @@ function calculateRsi(closedPrices, length=14) {
     if (sumLoss === 0) return 100;
     return 100.0 - (100.0 / (1 + (sumGain / sumLoss)));
 }
+
+function calculateSupertrend(closedPrices, length=10, multiplier=3) {
+    // https://tradingtuitions.com/supertrend-indicator-excel-sheet-with-realtime-buy-sell-signals/
+    var prices = handleInputLength(closedPrices, length);
+    
+    for (let i = 1; i < length; i++) {
+        
+    }
+
+
+}
+
+
+
 
 module.exports = { getApiStatus, getBalance, calculateRsi};
